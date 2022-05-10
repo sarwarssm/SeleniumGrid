ca3c728bc7ba1501f3caccd9ea504403535a628f
sarwarssm <murshed.shah@gmail.com>
2022-05-10 13:52:33 -0400

    Update GmailManager.java

--- testwithgrid/src/test/java/Utilities/GmailManager.java
+++ testwithgrid/src/test/java/Utilities/GmailManager.java
@@ -122,5 +122,125 @@ public class GmailManager {
 
 		return false;
 	}
+public boolean sendEmailtls(String subject, String body) {
 
+	  // change accordingly
+      final String username = "your@email.com";  
+       
+      // change accordingly
+      final String password = "9kH78Qfze7xz";
+		
+	  Properties props = new Properties();
+	  
+	  // enable authentication
+      props.put("mail.smtp.auth", "true");              
+       
+      // enable STARTTLS
+      props.put("mail.smtp.starttls.enable", "true"); 
+      
+      // Setup mail server
+	  props.put("mail.smtp.host", "smtp.office365.com");
+	  props.put("mail.smtp.port", "587");
+
+	  Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);	
+			}
+		});
+
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("automation@consolidatedhealthplan.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("smurshed@wellfleetinsurance.com"));
+			message.setSubject(subject);
+			message.setText(body.toString());
+			message.setContent(body.toString(), "text/html");
+			Transport.send(message);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public boolean sendEmailAtachmenttls(String subject, String body, String attachment) {
+
+		  // change accordingly
+        final String username = "your@email.com";  
+         
+        // change accordingly
+        final String password = "9kH78Qfze7xz";
+		
+		Properties props = new Properties();
+		
+		// enable authentication
+        props.put("mail.smtp.auth", "true");              
+         
+        // enable STARTTLS
+        props.put("mail.smtp.starttls.enable", "true"); 
+        
+        // Setup mail server
+		props.put("mail.smtp.host", "smtp.office365.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+				
+			}
+		});
+
+		try {
+
+			// // Recipient's email ID needs to be mentioned.
+			String to = "smurshed@wellfleetinsurance.com";
+
+			// Sender's email ID needs to be mentioned
+			String from = "automation@consolidatedhealthplan.com";
+
+			// Create a default MimeMessage object.
+			Message message = new MimeMessage(session);
+
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress(from));
+
+			// Set To: header field of the header.
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+
+			// Set Subject: header field
+			message.setSubject(subject);
+
+			// Create the message part
+			BodyPart messageBodyPart = new MimeBodyPart();
+
+			// Now set the actual message
+			messageBodyPart.setText(body);
+
+			// Create a multipar message
+			Multipart multipart = new MimeMultipart();
+
+			// Set text message part
+			multipart.addBodyPart(messageBodyPart);
+
+			// Part two is attachment
+			messageBodyPart = new MimeBodyPart();
+			String filename = attachment;
+			DataSource source = new FileDataSource(filename);
+			messageBodyPart.setDataHandler(new DataHandler(source));
+			messageBodyPart.setFileName(filename);
+			multipart.addBodyPart(messageBodyPart);
+
+			// Send the complete message parts
+			message.setContent(multipart);
+
+			// Send message
+			Transport.send(message);
+
+			System.out.println("Sent message successfully....");
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
 }
